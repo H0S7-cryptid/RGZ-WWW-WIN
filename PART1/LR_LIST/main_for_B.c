f649d5f973a02fd71fe4b2e1e84b00a80606d9d7
host1l3 <timvitkulevskij@gmail.com>
2025-06-09 13:04:56 +0700

    Update main_for_B.c

--- PART1/LR_LIST/main_for_B.c
+++ PART1/LR_LIST/main_for_B.c
@@ -2,15 +2,16 @@
 
 node* HeadTracker(node* head) {
 	char buffer[BUFF_SIZE];
-	char item = 0;
+	int choice = 0;
 
 	fgets(buffer, BUFF_SIZE, stdin);
-	if (IsTrueNumber(buffer) && atoi(buffer) < 4 && atoi(buffer) > 0) item = (char)atoi(buffer);
+	if (IsTrueNumber(buffer) == NUM_IS_GOOD) {
+		choice = atoi(buffer);
+	}
 
-	switch (item) {
+	switch (choice) {
 	case 1: {
-		TV tv;
-		tv = CreateUserTV();
+		TV tv = CreateUserTV();
 		head = Insert(head, tv);
 		break;
 	}
@@ -21,36 +22,44 @@ node* HeadTracker(node* head) {
 	case 3: {
 		printf("Enter delete position: ");
 		int pos = 0;
-		fgets(buffer, BUFF_SIZE, stdin);
-		if (IsTrueNumber(buffer)) pos = atoi(buffer);
-
+		if (fgets(buffer, BUFF_SIZE, stdin) != NULL && IsTrueNumber(buffer)) {
+			pos = atoi(buffer);
+		}
 		head = Delete(head, pos);
 		break;
 	}
+	default:
+		printf("Incorrect choice. Try again.\n");
+		break;
 	}
 	return head;
 }
 
 int main() {
-
 	node* head = NULL;
-
 	char buffer_for_item[BUFF_SIZE];
-	int item;
+	int stopEditing = 0;
 
 	printf("Work with Linked Ordered List.\n");
-	printf("Select action:\n1. Add new element\n2. Printf all list\n3. Delete element by position\n\n");
+	printf("Select action:\n");
+	printf("1. Add new element\n");
+	printf("2. Print entire list\n");
+	printf("3. Delete element by position\n\n");
+
 	while (1)
 	{
 		head = HeadTracker(head);
-		printf("Закончить работу со списком? (0 - Нет, 1\\Enter - Да)\n");
-		fgets(buffer_for_item, BUFF_SIZE, stdin); 
-		item = atoi(buffer_for_item);
-		if (item == 0 && head != NULL) continue;
-		else break;
+
+		printf("Stop editing the list? (0 - No, 1 - Yes): ");
+		fgets(buffer_for_item, BUFF_SIZE, stdin);
+		stopEditing = atoi(buffer_for_item); 
+
+		if (stopEditing == 0)
+			continue;
+		else
+			break;
 	}
 
 	DeleteList(&head);
-
 	return 0;
 }
